e94747cebf91a46381f4a32f2f7d4f5c3b423886
mohamedmostafadawood <mohamed.dawood@student.giu-uni.de>
2022-07-13 21:36:19 +0200

    properly displaying MAC

--- Sniffer/main.py
+++ Sniffer/main.py
@@ -2,7 +2,16 @@ import socket
 import struct
 import textwrap
 
-#unpacking ethernet frame
+
+# unpacking ethernet frame
+
 def unpackEthernetFrame(data):
     destMac, srcMac, protocol = struct.unpack("! 6s 6s H", data[:14])
     return getMacAddress(destMac), getMacAddress(srcMac), socket.htons(protocol), data[14:]
+
+
+# Return properly formatted MAC address (Example: FF:FF:FF:FF:FF:FF)
+
+def getMacAddress(macAddInBytes):
+    strMac = map(":02x".format, macAddInBytes)
+    return ':'.join(strMac).upper()
