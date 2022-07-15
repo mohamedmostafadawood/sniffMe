a445a905481cd67fb8740b2f92a86d6fe6974045
mohamedmostafadawood <mohamed.dawood@student.giu-uni.de>
2022-07-15 23:36:22 +0200

    add socket for ethernet frames and display them

--- Sniffer/main.py
+++ Sniffer/main.py
@@ -3,6 +3,23 @@ import struct
 import textwrap
 
 
+
+def main():
+    
+    #For windows users, please run it with administration privileges.
+    
+    HOST = socket.gethostbyname(socket.gethostname())
+    conn = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    conn.bind((HOST,0))
+    conn.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    conn.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    while True:
+        rawData, address = conn.recvfrom(65536)
+        destMac, srcMac, ethProtocol, data = unpackEthernetFrame(rawData)
+        print("\nEthernet Frame:")
+        print("DestinationMac {}, Source Mac {}, Ethernet Protocol {}".format(destMac, srcMac, ethProtocol))
+
 # unpacking ethernet frame
 
 def unpackEthernetFrame(data):
@@ -13,5 +30,8 @@ def unpackEthernetFrame(data):
 # Return properly formatted MAC address (Example: FF:FF:FF:FF:FF:FF)
 
 def getMacAddress(macAddInBytes):
-    strMac = map(":02x".format, macAddInBytes)
+    strMac = map("{:02x}".format, macAddInBytes)
     return ':'.join(strMac).upper()
+
+
+main()
