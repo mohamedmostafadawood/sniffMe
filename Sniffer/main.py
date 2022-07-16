155ec57bc75e4bc2b79b08a53e2f4636367db63c
mohamedmostafadawood <mohamed.dawood@student.giu-uni.de>
2022-07-16 02:08:14 +0200

    Extracting the IP header length and version with its documentation.

--- Sniffer/main.py
+++ Sniffer/main.py
@@ -1,3 +1,5 @@
+from email.base64mime import header_length
+from ensurepip import version
 import socket
 import struct
 import textwrap
@@ -17,10 +19,10 @@ def main():
     while True:
         rawData, address = conn.recvfrom(65536)
         destMac, srcMac, ethProtocol, data = unpackEthernetFrame(rawData)
-        print("\nEthernet Frame:")
-        print("DestinationMac {}, Source Mac {}, Ethernet Protocol {}".format(destMac, srcMac, ethProtocol))
-
-# unpacking ethernet frame
+        #print("\nEthernet Frame:")
+        #print("DestinationMac {}, Source Mac {}, Ethernet Protocol {}".format(destMac, srcMac, ethProtocol))
+        
+# Unpacking ethernet frame
 
 def unpackEthernetFrame(data):
     destMac, srcMac, protocol = struct.unpack("! 6s 6s H", data[:14])
@@ -32,6 +34,33 @@ def unpackEthernetFrame(data):
 def getMacAddress(macAddInBytes):
     strMac = map("{:02x}".format, macAddInBytes)
     return ':'.join(strMac).upper()
+
+
+# Unpacking IPv4 packets
+
+def unpackIPv4Packets(data):
+    version_header_length = data[0] #because they both are 8 bits which are the first byte of the data
+    version = version_header_length >> 4 #shift right by 4 to get the version only out of the combined 8 bits
+    
+    
+    # If anyone is wondering why did he use (version_header_length & 15 ). Here's the reason:
+    #lets assume: #version_header_length = 10101101. #15 = 00001111 # bitwise representation
+    #version_header_length & 15 = 00001101 which is same as 1101. So, we get the header_length only.
+    
+    #Header Length: this 4 bit field tells us the length of the IP header in 32 bit increments.
+    #The minimum length of an IP header is 20 bytes so with 32 bit increments, you would see value of 5 here.
+    # The maximum value we can create with 4 bits is 15 so with 32 bit increments, that would be a header length of 60 bytes.#This field is also called the Internet Header Length (IHL)
+    # So, you take this 4 bits field and convert it to decimal. The decimal number you have is how many words is our header_lenght.
+    # Then, I mutliply it by 4 to convert it from words to bytes to address the start of your payload properly.
+    header_length = (version_header_length & 15) * 4
+    
+    
+    
+
+
+
+
+
 
 
 main()
